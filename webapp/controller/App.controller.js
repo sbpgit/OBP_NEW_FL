2a32c370fcbc55375ef5ced3e0db1eee01b02d46
Pradeep Daka <pradeepkumardaka@sbpcorp.in>
2026-03-12 09:14:25 +0000

    Deployed app first time

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("obpnew.orderbasedplanningfl.controller.App", {
+      onInit() {
+      }
+  });
+});
